97cd4578e03eee4877496852f4976a3b734d5f1c
LAPTOP-H41J1R8O\Hp <iamrkgolu@gmail.com>
2021-09-21 13:08:37 +0530

    ----------------------------------------------------------------------------------

---
+++ ArrayListExample.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class ArrayListExample
+{
+	public static void main(String... b)
+	{
+		ArrayList<String>list=new ArrayList<String>();
+		list.add("Mango");
+		list.add("Apple");
+		list.add("Banana");
+		list.add("Grapes");
+		System.out.println(list);
+	}
+}
